32b86c0920e5ff8e2e216296455e6083b8a388a1
Sina Elahimanesh <59636948+SinaElahimanesh@users.noreply.github.com>
2023-04-28 09:42:55 +0330

    Add files via upload

---
+++ src/ChatApp.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import "./ChatApp.css";
+
+function ChatApp() {
+  const [message, setMessage] = useState('');
+  const [chat, setChat] = useState([]);
+
+  const sendMessage = async () => {
+    // setChat([...chat, { message, sender: 'user' }]);
+    // setMessage('');
+    const response = await axios.post('http://127.0.0.1:8000/api/', { message });
+    console.log(response)
+    setChat([...chat, { message, sender: 'user' }, { message: response.data.response, sender: 'bot' }]);
+    setMessage('');
+  };
+
+  const handleChange = (e) => {
+    if (e.key === "Enter") {
+      sendMessage(e);
+    }
+  };
+
+  useEffect(()=>{
+    async function fetchMyAPI() {
+      console.log('i fire once');
+      const response = await axios.post('http://127.0.0.1:8000/api/', { "message":"empty" });
+      console.log(response)
+      setChat([...chat, { message: response.data.response, sender: 'bot' }]);
+      setMessage('');
+    }
+
+    fetchMyAPI()
+   
+}, []);
+
+  return (
+    <div className="chat-container">
+        <div className="chat-banner">satbot</div>
+      <div className="chat-messages">
+        {chat.map((item, index) => (
+          <div key={index} className={`chat-message ${item.sender === 'user' ? 'chat-message-user' : ''}`}>
+            {item.sender === 'user' && (
+              <div className="chat-message-bubble  you">
+                <b>You:</b> {item.message}
+              </div>
+            )}
+            {item.sender === 'bot' && (
+              <div className="chat-message-bubble">
+                <b>SAT:</b> {item.message}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+      <div className="chat-input">
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) => handleChange(e)}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+    </div>
+  );
+}
+
+export default ChatApp;
